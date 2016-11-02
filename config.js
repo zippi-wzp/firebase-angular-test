471d500edb4db5af66ea93fa3e0cb53126f59b33
user <truebatmandc@gmail.com>
2016-11-02 17:30:27 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+(function () {
+  'use strict';
+  var config_module = angular.module('Form.config', [])
+	.constant('apiKey', 'AIzaSyCaLfRuTpxCF1G5VyqbKQaTRvXgvElrv7A')
+	.constant('databaseURL', 'https://test-task-737e1.firebaseio.com/');
+})();
---
+++ form.js
@@ -0,0 +1,49 @@
+(function () {
+  'use strict';
+  
+  angular.module('Form.config', [])
+  .constant('apiKey', 'AIzaSyCaLfRuTpxCF1G5VyqbKQaTRvXgvElrv7A')
+  .constant('databaseUrl', 'https://test-task-737e1.firebaseio.com/');
+  // .run(['apiKey', 'databaseUrl', function (apiKey, databaseUrl) {
+  //     firebase.initializeApp(apiKey, databaseUrl);
+  //   }]
+  // );
+
+  angular.module("Form", ["Form.config", "firebase"])
+  // .constant('apiKey', 'AIzaSyCaLfRuTpxCF1G5VyqbKQaTRvXgvElrv7A')
+  // .constant('databaseUrl', 'https://test-task-737e1.firebaseio.com/')
+  // .run('apiKey', 'databaseUrl', [function (apiKey, databaseUrl) {
+  //     firebase.initializeApp(apiKey, databaseUrl);
+  // }])
+  .factory('FireBaseService', [
+    function () {
+      return {
+        // initApp: function (apiKey, databaseURL) {
+        //   var config = {
+        //     apiKey: apiKey,
+        //     databaseURL: databaseURL
+        //   };
+        //   firebase.initializeApp(config);
+        // },
+        storeData: function (data) {
+          firebase.database().ref().child('messages').set(data);
+          //ref.set(data);
+        }
+      };
+    }
+  ])
+  .controller("FormController", ['$scope', '$firebase', 'apiKey', 'databaseURL', '$firebaseObject', 'FireBaseService', FormController]);
+
+  function FormController ($scope, $firebase, apiKey, databaseURL, $firebaseObject, FireBaseService) {
+    $scope.formData = {
+      email: '',
+      password: ''
+    }
+    // FireBaseService.initApp(apiKey, databaseURL);
+    // var ref = firebase.database().ref().child('messages');
+
+    $scope.sendFormData = function () {
+      FireBaseService.storeData($scope.formData, ref);
+    }
+  }
+})();
